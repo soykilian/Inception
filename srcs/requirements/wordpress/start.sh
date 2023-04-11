300a1cfdd01f8e6d3620c97fe331f68ef275f9a2
soykilian <mariavdacosta.27@gmail.com>
2023-04-11 20:23:25 +0200

    something broke

--- srcs/requirements/wordpress/start.sh
+++ srcs/requirements/wordpress/start.sh
@@ -2,13 +2,13 @@
 
 wp --allow-root core download
 
-wp --allow-root config create --dbhost=mariadb --dbname=$DBNAME --dbuser=$USERNAME --dbpass=$PASSWORD
+wp --allow-root config create --dbhost=mariadb --dbname=$DBNAME --dbuser=$USERNAME --dbpass=$DB_PASSWORD
 
 wp --allow-root db create
 
-wp --allow-root core install --url=mclerico.42.fr --title=Inception --admin_user=mclerico --admin_password=mclerico --admin_email=mclerico@student.42madrid.com
+wp --allow-root core install --url=mclerico.42.fr --title=Inception --admin_user=$WP_ADMIN_NAME --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_EMAIL
 
-wp --allow-root user create mavi mclerico@student.42madrid.com --user_pass=1234
+wp --allow-root user create $WP_USER_NAME $WP_USER_EMAIL --user_pass=$WP_USER_PASS
 
 # Execute PHP
 php-fpm7.3 -F
